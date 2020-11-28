2eee419474edc740dd1767d41ecc1f6c11e76b4c
gojaebeom <const.gjb@gmai.com>
2020-11-28 15:26:50 +0900

    item list output view

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,12 +1,17 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
 
 
 def hello_world(request):
+
+    # hello world list
+    hello_world_list = HelloWorld.objects.all()
+
     if request.method == "POST":
         temp = request.POST.get('data1')
 
@@ -14,6 +19,6 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
-        return render(request, 'hello.html', context={'new_hello_world': new_hello_world})
-    else :
-        return render(request, 'hello.html', context={'text': 'GET METHOD!'})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+    else:
+        return render(request, 'hello.html', context={'hello_world_list': hello_world_list})
