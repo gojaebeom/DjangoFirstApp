34ab2d07033c727f11d6669cad372a7e6480ef99
gojaebeom <const.gjb@gmai.com>
2020-11-28 15:12:37 +0900

    post data mysql save exam

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,8 +3,17 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from accountapp.models import HelloWorld
+
+
 def hello_world(request):
     if request.method == "POST":
-        return render(request, 'hello.html', context={'text': 'POST METHOD!'})
+        temp = request.POST.get('data1')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return render(request, 'hello.html', context={'new_hello_world': new_hello_world})
     else :
         return render(request, 'hello.html', context={'text': 'GET METHOD!'})
